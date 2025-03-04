9673a929418e1a58e15f7a62874b481733bb9aae
Zach <zowen17@gmail.com>
2025-03-04 00:46:38 -0500

    fixed auth code

---
+++ server/dist/routes/auth-routes.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+    // TODO: If the user exists and the password is correct, return a JWT token
+    try {
+        const user = await User.findOne({ where: { username } });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        const token = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1h',
+        });
+        res.json({ token });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' }); // Handle server errors
+    }
+};
+const router = Router();
+// POST /login - Login a user
+router.post('/login', login);
+export default router;
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,23 +1,31 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
-  const token = req.headers['authorization']?.split(' ')[1];
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
-    return res.sendStatus(401);
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401);
   }
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user: JwtPayload | undefined) => {
-    if (err) {
-      return res.sendStatus(403);
-    }
-    req.user = user;
-    next();
-  });
 };
